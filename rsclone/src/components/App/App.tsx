a938950739ffa939df629a9a38161b8af6fa8402
KantyshVitali <kantyshvitali@gmail.com>
2021-01-25 00:57:25 +0300

    feat: add Route

--- rsclone/src/components/App/App.tsx
+++ rsclone/src/components/App/App.tsx
@@ -1,7 +1,29 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
-import Board from '@/components/Board/Board';
+import BoardList from '@/components/BoardList';
 
-const App = (): JSX.Element => <Board />;
+const App = () => (
+  <Router>
+    <div>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/boardList">board list</Link>
+        </li>
+      </ul>
+      <hr />
+      <Switch>
+        <Route exact path="/" />
+        <Route path="/boardList">
+          <BoardList />
+        </Route>
+      </Switch>
+    </div>
+  </Router>
+);
 
-export default App;
+export default connect()(App);
