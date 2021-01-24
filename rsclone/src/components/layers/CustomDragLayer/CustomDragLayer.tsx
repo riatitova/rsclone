5d716ffcb6f32fc63d3c9ae163ffee444d5fce0d
KantyshVitali <kantyshvitali@gmail.com>
2021-01-25 01:09:50 +0300

    feat: custom drag layer

--- rsclone/src/components/layers/CustomDragLayer/CustomDragLayer.tsx
+++ rsclone/src/components/layers/CustomDragLayer/CustomDragLayer.tsx
@@ -1,29 +1,27 @@
 import React from 'react';
-import { XYCoord, useDragLayer } from 'react-dnd';
+import { useDragLayer } from 'react-dnd';
 
 import { CustomDragLayerContainer } from '@/assets/stylesheets/styles';
-import Card from '@/components/Board/Card';
-import Column from '@/components/Board/Column';
 
-function getItemStyles(currentOffset: XYCoord | null) {
-  if (!currentOffset) {
-    return {
-      display: 'none',
-    };
-  }
+// function getItemStyles(currentOffset: XYCoord | null) {
+//   if (!currentOffset) {
+//     return {
+//       display: 'none',
+//     };
+//   }
 
-  const { x, y } = currentOffset;
+//   const { x, y } = currentOffset;
 
-  const transform = `translate(${x}px, ${y}px)`;
-  return {
-    transform,
-    WebkitTransform: transform,
-  };
-}
+//   const transform = `translate(${x}px, ${y}px)`;
+//   return {
+//     transform,
+//     WebkitTransform: transform,
+//   };
+// }
 
 const CustomDragLayer: React.FC = () => {
   // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-  const { isDragging, item, currentOffset } = useDragLayer(monitor => ({
+  const { isDragging } = useDragLayer(monitor => ({
     item: monitor.getItem(), // eslint-disable-line @typescript-eslint/no-unsafe-assignment
     currentOffset: monitor.getSourceClientOffset(),
     isDragging: monitor.isDragging(),
@@ -33,27 +31,6 @@ const CustomDragLayer: React.FC = () => {
     return null;
   }
 
-  return (
-    <CustomDragLayerContainer>
-      <div style={getItemStyles(currentOffset)}>
-        {item.type === 'COLUMN' ? ( // eslint-disable-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
-          <Column
-            id={item.id} // eslint-disable-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
-            text={item.text} // eslint-disable-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
-            index={item.index} // eslint-disable-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
-            isPreview
-          />
-        ) : (
-          <Card
-            columnId={item.columnId} // eslint-disable-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
-            isPreview
-            index={0}
-            id={item.id} // eslint-disable-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
-            text={item.text} // eslint-disable-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
-          />
-        )}
-      </div>
-    </CustomDragLayerContainer>
-  );
+  return <CustomDragLayerContainer />;
 };
 export default CustomDragLayer;
