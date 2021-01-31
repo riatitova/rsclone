0832e6d3d970d2bb5285c16c52bed128f4ede925
KantyshVitali <kantyshvitali@gmail.com>
2021-02-01 00:23:41 +0300

    feat: add props on card menu

--- rsclone/src/components/Board/ColumnCard/CardMenu/CardMenu.tsx
+++ rsclone/src/components/Board/ColumnCard/CardMenu/CardMenu.tsx
@@ -1,41 +1,77 @@
-import React from 'react';
+import React, { Dispatch } from 'react';
+import { connect } from 'react-redux';
 
 import style from '@/components/Board/ColumnCard/CardMenu/CardMenu.scss';
+import { IBoardList } from '@/constants/index';
+import { deleteCard } from '@/store/actions/actions';
+import { RootState } from '@/store/reducers/rootReducer';
 
 interface CardMenuProps {
   text: string;
   closePopup: () => void;
-
-  // taskId: string;
-  // columnId: string;
+  cardId: string;
+  columnId: string;
+  boardId: string;
 }
 
-type Props = CardMenuProps;
+interface DispatchProps {
+  onDeleteCard: (cardId: string, boardId: string, columnId: string) => void;
+}
 
-const CardMenu: React.FC<Props> = (props: Props) => (
-  <div className={style.popup}>
-    <div className={style.card__menu_visible}>
-      <h3 className={style.card__name}>
-        Current task:
-        {props.text}
-      </h3>
-      <div>
-        <h3>Description: </h3>
-        <textarea className={style.card__description} placeholder="Card Description" />
-      </div>
-      <div className={style.card__date}>
-        <h3>Date: </h3>
-        <span>{Date()}</span>
-      </div>
-      <div className={style.card__buttons}>
-        <button type="button" className={style.card__button}>
-          Delete card
-        </button>
-        <button type="button" className={style.card__button} onClick={props.closePopup}>
-          Close
-        </button>
+interface StateProps {
+  // eslint-disable-next-line react/no-unused-prop-types
+  board: IBoardList[];
+}
+
+type Props = StateProps & CardMenuProps & DispatchProps;
+
+const CardMenu: React.FC<Props> = (props: Props) => {
+  const deleteCardFunc = () => {
+    props.onDeleteCard(props.cardId, props.boardId, props.columnId);
+    props.closePopup();
+  };
+
+  return (
+    <div className={style.popup}>
+      <div className={style.card__menu_visible}>
+        <h3 className={style.card__name}>
+          Current task:
+          {props.text}
+        </h3>
+        <div>
+          <h3>Description: </h3>
+          <textarea className={style.card__description} placeholder="Card Description" />
+        </div>
+        <div className={style.card__date}>
+          <h3>Date: </h3>
+          <span>{Date()}</span>
+        </div>
+        <div className={style.card__buttons}>
+          <button type="button" className={style.card__button} onClick={deleteCardFunc}>
+            Delete card
+          </button>
+          <button type="button" className={style.card__button} onClick={props.closePopup}>
+            Close
+          </button>
+        </div>
       </div>
     </div>
-  </div>
-);
-export default CardMenu;
+  );
+};
+
+const mapStateToProps = (state: RootState) => {
+  const boardList: IBoardList[] = state.boardList?.boardList;
+  return {
+    board: boardList,
+  };
+};
+
+const mapDispatchToProps = (dispatch: Dispatch<any>) => ({
+  onDeleteCard: (cardId: string, boardId: string, columnId: string) =>
+    dispatch(deleteCard({ cardId, boardId, columnId })),
+});
+
+export default connect<StateProps, DispatchProps, CardMenuProps>(
+  mapStateToProps,
+  mapDispatchToProps
+)(CardMenu);
