fb1a1f16365579ebc613f02fb042519e1a9ffa10
KantyshVitali <kantyshvitali@gmail.com>
2021-02-01 16:33:05 +0300

    feat: add redux connect to boardList

--- rsclone/src/components/BoardList/BoardList.tsx
+++ rsclone/src/components/BoardList/BoardList.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import React, { useState, Dispatch } from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 import styles from '@/components/icons/BaseIcon/BaseIcon.scss';
@@ -16,9 +16,19 @@ import {
   CrossIconWrapper,
 } from './BoardList.styled';
 
-const BoardList = () => {
-  const dispatch = useDispatch();
-  const boards: IBoardList[] = useSelector((state: RootState) => state.boardList?.boardList);
+interface DispatchProps {
+  onAddBoard: (text: string) => void;
+  onDeleteBoard: (boardId: string) => void;
+}
+
+interface StateProps {
+  board: IBoardList[];
+}
+
+type Props = StateProps & DispatchProps;
+
+const BoardList = (props: Props) => {
+  const boards = props.board;
 
   const [boardName, setBoardName] = useState('');
 
@@ -26,13 +36,13 @@ const BoardList = () => {
     setBoardName(event.currentTarget.value);
   };
 
-  const onAddBoard = (str: string) => {
-    dispatch(addBoard({ text: str }));
+  const onAddBoardFunc = () => {
+    props.onAddBoard(boardName);
     setBoardName('');
   };
 
-  const onDeleteBoard = (boardId: string) => {
-    dispatch(deleteBoard({ boardId }));
+  const deleteBoardFunc = (boardId: string) => {
+    props.onDeleteBoard(boardId);
   };
 
   return (
@@ -40,7 +50,7 @@ const BoardList = () => {
       {boards?.map(value => (
         <BlockWrapper key={value.boardId}>
           <CrossIconWrapper>
-            <CrossIcon className={styles.size_l} onClick={() => onDeleteBoard(value.boardId)} />
+            <CrossIcon className={styles.size_l} onClick={() => deleteBoardFunc(value.boardId)} />
           </CrossIconWrapper>
           <Link key={value.boardId} to={`/board_${value.boardId}`}>
             {value.boardName}
@@ -58,8 +68,7 @@ const BoardList = () => {
             value={boardName}
             onChange={changeName}
           />
-          {/* // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment*/}
-          <button type='submit' onClick={onAddBoard.bind(this, boardName)}>
+          <button type="submit" onClick={onAddBoardFunc}>
             +
           </button>
         </StyledBoardInputWrapper>
@@ -68,4 +77,16 @@ const BoardList = () => {
   );
 };
 
-export default BoardList;
+const mapStateToProps = (state: RootState) => {
+  const boardList: IBoardList[] = state.boardList?.boardList;
+  return {
+    board: boardList,
+  };
+};
+
+const mapDispatchToProps = (dispatch: Dispatch<any>) => ({
+  onAddBoard: (text: string) => dispatch(addBoard({ text })),
+  onDeleteBoard: (boardId: string) => dispatch(deleteBoard({ boardId })),
+});
+
+export default connect<StateProps, DispatchProps>(mapStateToProps, mapDispatchToProps)(BoardList);
